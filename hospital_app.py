89d82e15ef639f5ea4d20d70a47dbca1460a7fca
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-03 13:43:07 +0700

    Update

--- hospital_app.py
+++ hospital_app.py
@@ -206,23 +206,23 @@ with st.form("triage_form"):
     # Section 5 — Patient Info
     st.markdown("""
     <!-- COLOR6 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#09f61d;border:1px solid #0ddc71;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#0adbf2;color:rgb(0, 0, 0);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">5</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">CODENO24</span>
+            <span style="font-size:16px;font-weight:600;color:#fa10b8;">Patint Information</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
 
     col_age, col_gen = st.columns(2)
     with col_age:
-        age    = st.number_input("CODENO25", min_value=1, max_value=120, value=35)
+        age    = st.number_input("", min_value=1, max_value=120, value=35)
     with col_gen:
-        gender = st.selectbox("CODENO26", options=['Female', 'Male'])
+        gender = st.selectbox("Gender", options=['Female', 'Male'])
 
-    submitted = st.form_submit_button("CODENO27 →")
+    submitted = st.form_submit_button("Get AI information →")
 
 # ── Result ────────────────────────────────────────────────────────────────────
 if submitted:
